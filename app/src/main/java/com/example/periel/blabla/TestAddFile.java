a1af7748cc219889f1400a8b9077a9f8a114a977
Periel <periel1001@gmail.com>
2016-12-01 10:11:54 +0200

    add file and one method add method in main activity

---
+++ app/src/main/java/com/example/periel/blabla/TestAddFile.java
@@ -0,0 +1,20 @@
+package com.example.periel.blabla;
+
+import android.widget.Toast;
+
+/**
+ * Created by Periel on 12/1/2016.
+ */
+
+public class TestAddFile {
+
+    public TestAddFile() {
+    }
+
+    private void myname() {
+        String myNAme = "periel";
+
+        System.out.print(myNAme);
+
+    }
+}
